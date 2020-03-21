52044313017708217c4b24430af707642280c0a7
nour <nour.saffaf@gmail.com>
2020-03-21 16:38:51 +0200

    Initial Commit

---
+++ MemoryCardsGameAR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemoryCardsGameAR
+//
+//  Created by Nour on 21.3.2020.
+//  Copyright © 2020 Nour Saffaf. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
